a0b7ce6cb2d996008cae83a3aedab51d87cf5351
Jake Jasmin <roastbeefer000@gmail.com>
2025-03-18 21:14:03 -0600

    push notifications work

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,53 @@
+import 'dart:developer';
+
 import 'package:english_words/english_words.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  // await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  final notificationSettings = await FirebaseMessaging.instance.requestPermission(provisional: true);
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+// void main() {
+//   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+//   runApp(MyApp());
+// }
 
 void main() {
-  runApp(MyApp());
+    WidgetsFlutterBinding.ensureInitialized();
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+    // print("hello");
+    // await Firebase.initializeApp(
+    //         options: DefaultFirebaseOptions.currentPlatform,
+    //         );
+    // final notificationSettings = await FirebaseMessaging.instance.requestPermission(provisional: true);
+    // final token = await FirebaseMessaging.instance.getToken();
+    // print("Token: $token");
+
+    runApp(const MyApp());
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+            print('Got a message whilst in the foreground!');
+            print('Message data: ${message.data}');
+
+            if (message.notification != null) {
+            print('Message also contained a notification: ${message.notification}');
+            }
+            });
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
